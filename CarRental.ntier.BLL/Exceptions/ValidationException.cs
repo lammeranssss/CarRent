48a82c4be3adc0fe880b04b536666efed54e5784
Taiga <kosonogov2005@gmail.com>
2025-08-26 11:35:43 +0300

    fix

---
+++ CarRental.ntier.BLL/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRental.ntier.BLL.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public Dictionary<string, string[]> Errors { get; }
+
+        public ValidationException(Dictionary<string, string[]> errors)
+            : base("Validation failed")
+        {
+            Errors = errors;
+        }
+    }
+}
