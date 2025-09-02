8adc816c3b914e41e09ccb3dc44ee1f130e22857
Taiga <kosonogov2005@gmail.com>
2025-09-02 16:30:14 +0300

    add generic service

---
+++ CarRental.BLL/Abstractions/IGenericService.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRental.BLL.Abstractions;
+
+public interface IGenericService<TModel>
+{
+    Task<TModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<TModel>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<TModel> AddAsync(TModel model, CancellationToken cancellationToken = default);
+    Task<TModel> UpdateAsync(TModel model, CancellationToken cancellationToken = default);
+    Task RemoveAsync(Guid id, CancellationToken cancellationToken = default);
+}
--- CarRental.BLL/DI/DependencyRegistrar.cs
+++ CarRental.BLL/DI/DependencyRegistrar.cs
@@ -1,4 +1,6 @@
-﻿using CarRental.DAL.DI;
+﻿using CarRental.BLL.Abstractions;
+using CarRental.BLL.Services;
+using CarRental.DAL.DI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +14,7 @@ public static class DependencyRegistrar
     {
         services.AddAutoMapper(typeof(Mapping.BllMappingProfile));
         services.AddDalDependencies(configuration);
+        services.AddScoped(typeof(IGenericService<>), typeof(GenericService<,>));
         return services;
     }
 }
