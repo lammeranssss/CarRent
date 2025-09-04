b9a6d388ce31da968238de2de3a4138a6bd96e8e
Taiga <kosonogov2005@gmail.com>
2025-09-04 17:14:03 +0300

    add using

--- CarRental.DAL/DataContext/LocationRepository.cs
+++ CarRental.DAL/DataContext/LocationRepository.cs
@@ -1,5 +1,7 @@
 ﻿using CarRental.DAL.Models.Entities;
 
+using CarRental.DAL.Abstractions;
+
 namespace CarRental.DAL.DataContext;
 
 public class LocationRepository(CarRentalDbContext context) : GenericRepository<LocationEntity>(context), ILocationRepository
