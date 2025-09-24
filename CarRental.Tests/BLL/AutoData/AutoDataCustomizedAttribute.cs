2df42c19188ec10751528947488776514e8cd242
Taiga <kosonogov2005@gmail.com>
2025-09-24 18:04:01 +0300

    delete package moq

--- CarRental.Tests/BLL/AutoData/AutoDataCustomizedAttribute.cs
+++ CarRental.Tests/BLL/AutoData/AutoDataCustomizedAttribute.cs
@@ -1,7 +1,6 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoNSubstitute;
 using AutoFixture.Xunit2;
-using CarRental.BLL.Models;
 
 namespace CarRental.Tests.BLL.AutoData;
 
@@ -20,11 +19,6 @@ public class AutoDataCustomizedAttribute : AutoDataAttribute
             .ForEach(b => fixture.Behaviors.Remove(b));
         fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
-        fixture.Customize<BookingModel>(c => c.Without(p => p.Customer).Without(p => p.Car).Without(p => p.Rental));
-        fixture.Customize<CarModel>(c => c.Without(p => p.Location).Without(p => p.Bookings));
-        fixture.Customize<CustomerModel>(c => c.Without(p => p.Bookings));
-        fixture.Customize<LocationModel>(c => c.Without(p => p.Cars).Without(p => p.PickUpRentals).Without(p => p.DropOffRentals));
-        fixture.Customize<RentalModel>(c => c.Without(p => p.Booking).Without(p => p.PickUpLocation).Without(p => p.DropOffLocation));
         return fixture;
     })
     {
