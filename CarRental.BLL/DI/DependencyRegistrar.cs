fa85759051b2b25715b7f8f1387dfa2db4cc4152
Taiga <kosonogov2005@gmail.com>
2025-10-07 15:17:35 +0300

    change mapping configuration and add scope for every service

--- CarRental.BLL/DI/DependencyRegistrar.cs
+++ CarRental.BLL/DI/DependencyRegistrar.cs
@@ -3,6 +3,7 @@ using CarRental.BLL.Services;
 using CarRental.DAL.DI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
 
 namespace CarRental.BLL.DI;
 
@@ -12,9 +13,13 @@ public static class DependencyRegistrar
         this IServiceCollection services, 
         IConfiguration configuration)
     {
-        services.AddAutoMapper(typeof(Mapping.BllMappingProfile));
+        services.AddAutoMapper(cfg => { cfg.AddMaps(typeof(Mapping.BllMappingProfile).Assembly); });
         services.AddDalDependencies(configuration);
-        services.AddScoped(typeof(IGenericService<>), typeof(GenericService<,>));
+        services.AddScoped<ICarService, CarService>();
+        services.AddScoped<ICustomerService, CustomerService>();
+        services.AddScoped<ILocationService, LocationService>();
+        services.AddScoped<IBookingService, BookingService>();
+        services.AddScoped<IRentalService, RentalService>();
         return services;
     }
 }
