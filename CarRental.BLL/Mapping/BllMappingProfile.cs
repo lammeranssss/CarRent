23dda00a14b73620e0972b1f2ad7167a4240b89f
Taiga <kosonogov2005@gmail.com>
2025-10-07 15:16:41 +0300

    add ignore options to mapping

--- CarRental.BLL/Mapping/BllMappingProfile.cs
+++ CarRental.BLL/Mapping/BllMappingProfile.cs
@@ -13,5 +13,23 @@ public class BllMappingProfile : Profile
         CreateMap<LocationEntity, LocationModel>().ReverseMap();
         CreateMap<RentalEntity, RentalModel>().ReverseMap();
         CreateMap<BookingEntity, BookingModel>().ReverseMap();
+        CreateMap<CarModel, CarEntity>()
+                    .ForMember(dest => dest.Location, opt => opt.Ignore());
+
+        CreateMap<CustomerModel, CustomerEntity>()
+            .ForMember(dest => dest.Bookings, opt => opt.Ignore());
+
+        CreateMap<LocationModel, LocationEntity>()
+            .ForMember(dest => dest.Cars, opt => opt.Ignore());
+
+        CreateMap<RentalModel, RentalEntity>()
+            .ForMember(dest => dest.Booking, opt => opt.Ignore())
+            .ForMember(dest => dest.PickUpLocation, opt => opt.Ignore())
+            .ForMember(dest => dest.DropOffLocation, opt => opt.Ignore());
+
+        CreateMap<BookingModel, BookingEntity>()
+            .ForMember(dest => dest.Car, opt => opt.Ignore())
+            .ForMember(dest => dest.Customer, opt => opt.Ignore())
+            .ForMember(dest => dest.Rental, opt => opt.Ignore());
     }
 }
